724383307d5f1e74e2cec4522f5df594a7e5436a
noori <82028072+noori1995@users.noreply.github.com>
2025-09-16 12:13:00 +0330

    init project

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">Hello Nuxt 3 + Tailwind 🚀</h1>
+    <NuxtLink to="/about" class="mt-4 text-lg text-gray-700 hover:text-black">
+      Go to About
+    </NuxtLink>
+  </div>
+</template>
