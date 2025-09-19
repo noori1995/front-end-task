203b7ce00d6e2e554c0847e4e3b496532a266597
noori <82028072+noori1995@users.noreply.github.com>
2025-09-19 12:26:47 +0330

    feat: add reusable icon components

---
+++ components/Icon/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+}>()
+
+const iconComponent = computed(() => {
+  return defineAsyncComponent(() => import(`~/components/icon/${props.name}.vue`))
+})
+</script>
---
+++ components/Icon/Upload.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg
+    width="25"
+    height="25"
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_763_247)">
+      <path
+        d="M4.5 17.5V19.5C4.5 20.0304 4.71071 20.5391 5.08579 20.9142C5.46086 21.2893 5.96957 21.5 6.5 21.5H18.5C19.0304 21.5 19.5391 21.2893 19.9142 20.9142C20.2893 20.5391 20.5 20.0304 20.5 19.5V17.5"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.5 9.5L12.5 4.5L17.5 9.5"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.5 4.5V16.5"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_763_247">
+        <rect
+          width="24"
+          height="24"
+          fill="white"
+          transform="translate(0.5 0.5)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
